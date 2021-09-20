4542ba069e3d67a53ce2a6ef74144bb2b1caa5c5
awenzelhuemer <43669899+Superandi1234@users.noreply.github.com>
2021-09-20 14:36:31 +0200

    Add shared dependencies

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,6 +3,16 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
 
+  singleSpaWebpackConfig.externals.push(
+    // '@angular/common',
+    // '@angular/core',
+    // '@angular/platform-browser',
+    // '@angular/router',
+    'rxjs',
+    'rxjs/operators',
+    'single-spa'
+  );
+
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
 };
