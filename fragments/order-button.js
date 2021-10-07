2b625b9f5b3645ba997895212ccfab8f7792ddc3
awenzelhuemer <43669899+Superandi1234@users.noreply.github.com>
2021-10-07 17:53:00 +0200

    Add order button fragment

---
+++ fragments/order-button.js
@@ -0,0 +1,27 @@
+class OrderAddButton extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <button class="btn btn-sm btn-secondary">
+            <i class="bi-plus"></i>
+        </button>`
+        this.querySelector("button").addEventListener("click", () => {
+            const id = this.getAttribute("id");
+            this.addItem(id);
+        });
+    }
+
+    addItem(id) {
+        const result = localStorage.getItem("checkout");
+        let items = result === null ? [] : JSON.parse(result);
+        items.push(id);
+
+        localStorage.setItem("checkout", JSON.stringify(items));
+
+        const event = new CustomEvent("order:item_added", {
+            bubbles: true,
+            detail: { id }
+        });
+        window.dispatchEvent(event);
+    }
+}
+window.customElements.define("order-add-button", OrderAddButton);
